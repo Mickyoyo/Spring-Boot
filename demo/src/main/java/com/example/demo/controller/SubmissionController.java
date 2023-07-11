8bac8650c83e1e8bae0fde2b4534731dadee7b9f
narain <tatanarain@gmail.com>
2023-07-11 15:55:09 -0500

    spring-boot

---
+++ demo/src/main/java/com/example/demo/controller/SubmissionController.java
@@ -0,0 +1,52 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.SubmissionDTO;
+import com.example.demo.service.SubmissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+
+
+@RestController
+@RequestMapping ("/submission")
+public class SubmissionController {
+
+    @Autowired
+    private SubmissionService submissionService;
+
+    @GetMapping("get/{id}")
+    public SubmissionDTO getSubmission(@PathVariable String id) {
+        return submissionService.getSubmission(id);
+
+    }
+
+    @GetMapping("/getAll")
+    public List<SubmissionDTO> getAll(){
+        return submissionService.getAll();
+    }
+
+    @PostMapping(path="/add")
+    public ResponseEntity<SubmissionDTO> addSubmission(@RequestBody SubmissionDTO submission) {
+        ResponseEntity<SubmissionDTO> re= new ResponseEntity<>(HttpStatus.CREATED);
+        re=ResponseEntity.status(re.getStatusCode()).body(submissionService.addSubmission(submission));
+
+        return re;
+
+    }
+
+    @PutMapping("update")
+    public SubmissionDTO updateSubmission( @RequestBody SubmissionDTO submission) {
+        return  submissionService.updateSubmission(submission);
+
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity deleteSubmission(@PathVariable String id) {
+        submissionService.deleteSubmission(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null) ;
+
+    }
+
+}
---
+++ demo/src/main/java/com/example/demo/service/SubmissionService.java
@@ -0,0 +1,30 @@
+package com.example.demo.service;
+
+import com.example.demo.model.SubmissionDTO;
+
+import java.util.List;
+
+public class SubmissionService {
+    public SubmissionDTO getSubmission(String id) {
+        return null;
+    }
+
+    public SubmissionDTO addSubmission(SubmissionDTO submission) {
+        return null;
+    }
+
+    public SubmissionDTO updateSubmission(SubmissionDTO submission) {
+        return null;
+    }
+
+    public boolean deleteSubmission(String id) {
+        return false;
+    }
+
+    public List<SubmissionDTO> getAll() {
+        return null;
+    }
+
+
+}
+
